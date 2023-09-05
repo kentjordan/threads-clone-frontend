ce19cfe2af33dec451d9bd5117c6e963c8dfa741
Kent Jordan <kentaki.dev@gmail.com>
2023-09-05 21:13:07 +0800

    Refactored authdAPI -> securedAPI

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-let authdAPI = axios.create({ baseURL: process.env.EXPO_PUBLIC_API_URL });
+let securedAPI = axios.create({ baseURL: process.env.EXPO_PUBLIC_API_URL });
 let publicAPI = axios.create({ baseURL: process.env.EXPO_PUBLIC_API_URL });
 
 export {
-    authdAPI,
+    securedAPI,
     publicAPI
 };
